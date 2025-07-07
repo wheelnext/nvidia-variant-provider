a2718c12e186ed405cf4ae8f493ce925d08ab419
Jonathan Dekhtiar <jonathan@dekhtiar.com>
2025-07-07 18:17:04 -0400

    Detect CUDA update

--- nvidia_variant_provider/detect_cuda.py
+++ nvidia_variant_provider/detect_cuda.py
@@ -1,5 +1,3 @@
-# Copyright (C) 2012 Anaconda, Inc
-# SPDX-License-Identifier: BSD-3-Clause
 """Detect CUDA version."""
 
 from __future__ import annotations
@@ -10,26 +8,11 @@ from dataclasses import dataclass
 
 import nvidia_variant_provider.pynvml as _pynvml
 
-NVIDIA_GPU_ARCHITECTURE = {
-    _pynvml.NVML_DEVICE_ARCH_KEPLER: "Kepler",  # example: Tesla K80
-    _pynvml.NVML_DEVICE_ARCH_MAXWELL: "Maxwell",  # example: Tesla M40
-    _pynvml.NVML_DEVICE_ARCH_PASCAL: "Pascal",  # example: Tesla P100
-    _pynvml.NVML_DEVICE_ARCH_VOLTA: "Volta",  # example: Tesla V100, Titan V
-    _pynvml.NVML_DEVICE_ARCH_TURING: "Turing",  # example: GeForce RTX 2080 Ti, Tesla T4
-    _pynvml.NVML_DEVICE_ARCH_AMPERE: "Ampere",  # example: GeForce RTX 3080, A100
-    _pynvml.NVML_DEVICE_ARCH_ADA: "Ada",  # example: GeForce RTX 4090, L40
-    _pynvml.NVML_DEVICE_ARCH_HOPPER: "Hopper",  # example: H100, H800
-    _pynvml.NVML_DEVICE_ARCH_BLACKWELL: "Blackwell",  # example: B100
-    _pynvml.NVML_DEVICE_ARCH_T23X: "Tegra",  # Tegra SKUs
-    _pynvml.NVML_DEVICE_ARCH_UNKNOWN: "Unknown",  # Error
-}
-
 
 @dataclass(frozen=True, order=True)
 class CudaEnvironment:
     system_driver_versions: str | None
     cuda_driver_version: str | None
-    gpu_families: list[str]
     architectures: list[str]
 
     @classmethod
@@ -39,10 +22,10 @@ class CudaEnvironment:
         try:
             _pynvml.nvmlInit()
 
-            # UMD / KMD Version
+            # KMD Version: (e.g. 525.85.12)
             system_driver_version = _pynvml.nvmlSystemGetDriverVersion()
 
-            # CUDA Driver
+            # UMD / LIBCUDA Driver Version: (e.g. 12.5.1)
             cuda_driver_version = None
             try:
                 cuda_driver_version = str(_pynvml.nvmlSystemGetCudaDriverVersion_v2())
@@ -58,17 +41,10 @@ class CudaEnvironment:
                         f"{(int(cuda_driver_version) % 1000) // 10}"
                     )
 
-            gpu_families: set[str] = set()
             architectures: set[str] = set()
 
             for device_id in range(_pynvml.nvmlDeviceGetCount()):
                 device = _pynvml.nvmlDeviceGetHandleByIndex(device_id)
-                with contextlib.suppress(_pynvml.NVMLError, KeyError):
-                    arch = NVIDIA_GPU_ARCHITECTURE[
-                        _pynvml.nvmlDeviceGetArchitecture(device=device)
-                    ]
-                    gpu_families.add(arch)
-
                 with contextlib.suppress(_pynvml.NVMLError):
                     cc = _pynvml.nvmlDeviceGetCudaComputeCapability(handle=device)
                     architectures.add(f"sm_{''.join([str(i) for i in cc])}")
@@ -76,7 +52,6 @@ class CudaEnvironment:
             return cls(
                 system_driver_versions=system_driver_version,
                 cuda_driver_version=cuda_driver_version,
-                gpu_families=list(gpu_families),
                 architectures=list(architectures),
             )
 
--- nvidia_variant_provider/plugin.py
+++ nvidia_variant_provider/plugin.py
@@ -1,11 +1,53 @@
 from __future__ import annotations
 
 import os
+import warnings
+from abc import abstractmethod
+from collections import defaultdict
 from dataclasses import dataclass
 from functools import cached_property
+from typing import Any
+from typing import Protocol
+from typing import runtime_checkable
+
+from packaging.specifiers import InvalidSpecifier
+from packaging.specifiers import SpecifierSet
+from packaging.version import Version
+from packaging.version import parse as parse_version
 
 from nvidia_variant_provider.detect_cuda import NVIDIA_GPU_ARCHITECTURE
 from nvidia_variant_provider.detect_cuda import CudaEnvironment
+from nvidia_variant_provider.version_sort import sort_specifier_sets
+
+FeatureIndex = int
+PropertyIndex = int
+
+VariantNamespace = str
+VariantFeatureName = str
+VariantFeatureValue = str
+
+
+@runtime_checkable
+class VariantPropertyType(Protocol):
+    """A protocol for variant properties"""
+
+    @property
+    @abstractmethod
+    def namespace(self) -> VariantNamespace:
+        """Namespace (from plugin)"""
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def feature(self) -> VariantFeatureName:
+        """Feature name (within the namespace)"""
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def value(self) -> VariantFeatureValue:
+        """Feature value"""
+        raise NotImplementedError
 
 
 @dataclass(frozen=True)
@@ -17,15 +59,28 @@ class VariantFeatureConfig:
 
 
 DRIVER_KEY = "driver"
-CUDA_KEY = "cuda"
-GPU_FAMILY_KEY = "gpu_family"
+CUDA_KEY = "cuda_version"
 SM_ARCH_KEY = "sm_arch"
+CTK_KEY = "ctk"
 
 LATEST_CUDA_MINOR_VERSIONS = {11: 8, 12: 9}
 
 
+class InputPreservingSpecifierSet(SpecifierSet):
+    original_value: str
+
+    def __init__(
+        self,
+        specifiers: str = "",
+        prereleases: bool | None = None,
+    ) -> None:
+        self.original_value = specifiers
+        super().__init__(specifiers=specifiers, prereleases=prereleases)
+
+
 class NvidiaVariantPlugin:
     namespace = "nvidia"
+    dynamic = True
 
     @cached_property
     def _cuda_environment(self) -> CudaEnvironment | None:
@@ -56,16 +111,6 @@ class NvidiaVariantPlugin:
         return cuda_env.cuda_driver_version
 
     @property
-    def gpu_families(self) -> list[str] | None:
-        if gpu_families := os.environ.get("NV_VARIANT_PROVIDER_FORCE_GPU_FAMILIES"):
-            return gpu_families.split(",")
-
-        if (cuda_env := self._cuda_environment) is None:
-            return None
-
-        return cuda_env.gpu_families
-
-    @property
     def architectures(self) -> list[str] | None:
         if architectures := os.environ.get(
             "NV_VARIANT_PROVIDER_FORCE_GPU_ARCHITECTURES"
@@ -77,68 +122,106 @@ class NvidiaVariantPlugin:
 
         return cuda_env.architectures
 
-    def get_supported_configs(self) -> list[VariantFeatureConfig]:
-        keyconfigs = []
+    def get_supported_configs(
+        self, known_properties: frozenset[VariantPropertyType] | None
+    ) -> list[VariantFeatureConfig]:
+        """Filter and sort the properties based on the plugin's logic."""
+
+        # 1.A - Validation: Validate input types.
+        assert isinstance(known_properties, frozenset)
+        assert all(isinstance(vprop, VariantPropertyType) for vprop in known_properties)
+
+        if not known_properties:
+            # Nothing In => Nothing Out
+            return []
+
+        # ============================================================================ #
+        # A. Preprocessing: Sort the properties by feature, and value.
+        # ============================================================================ #
+
+        prop_values: dict[VariantFeatureName, list[VariantFeatureValue]] = defaultdict(
+            list
+        )
+        for vprop in known_properties:
+            prop_values[vprop.feature].append(vprop.value)
+
+        # ============================================================================ #
+        # B. Validation: Ensure all properties belong to the proper namespace.
+        # ============================================================================ #
+
+        issues_found: list[str] = [
+            f"Property `{vprop}` does not belong to namespace {self.namespace}"
+            for vprop in known_properties
+            if vprop.namespace != self.namespace
+        ]
+        if issues_found:
+            raise ValueError(
+                f"Non compatible properties found in variant plugin "
+                f"`{self.namespace}`:"
+                + ("\n- " + ("\n- ".join(issues_found)) if issues_found else "")
+            )
+
+        # ============================================================================ #
+        # C. Processing: Filter and sort supported variant property values
+        # ============================================================================ #
+
+        keyconfigs: list[VariantFeatureConfig] = []
 
         # Priority 1: User-Mode Driver (UMD) Version
+
         if (umd_ver := self.umd_driver) is not None:
             keyconfigs.append(VariantFeatureConfig(name=DRIVER_KEY, values=[umd_ver]))
 
         # Priority 2: CUDA Driver Version
+
         if (cuda_ver := self.cuda_driver) is not None:
             keyconfigs.append(VariantFeatureConfig(name=CUDA_KEY, values=[cuda_ver]))
 
         # Priority 3: SM Architectures
-        if (architectures := self.architectures) is not None:
-            keyconfigs.append(
-                VariantFeatureConfig(name=SM_ARCH_KEY, values=architectures)
-            )
 
-        # Priority 4: GPU Families
-        if (gpu_families := self.gpu_families) is not None:
+        if architectures := prop_values.get(SM_ARCH_KEY, []):
+
+            def sort_key(item: str) -> tuple[int, int]:
+                num_str, type_str = item.split("_")
+                num = int(num_str)
+                # real = 0, virtual = 1 to prioritize 'real' first
+                type_priority = 0 if type_str == "real" else 1
+                return (-num, type_priority)
+
             keyconfigs.append(
                 VariantFeatureConfig(
-                    name=GPU_FAMILY_KEY, values=[s.lower() for s in gpu_families]
+                    name=SM_ARCH_KEY, values=sorted(architectures, key=sort_key)
+                )
+            )
+
+        # Priority 4: CTK
+        # - All versions supported by the plugin - purely sort by version
+        # - Used exclusively for dependency resolution
+
+        if ctks := prop_values.get(CTK_KEY, []):
+            keyconfigs.append(
+                VariantFeatureConfig(
+                    name=CTK_KEY,
+                    values=sorted(ctks, key=parse_version, reverse=True),
                 )
             )
 
         return keyconfigs
 
-    def get_all_configs(self) -> list[VariantFeatureConfig]:
-        # https://docs.nvidia.com/cuda/cuda-toolkit-release-notes/index.html
-        # The lowest supported UMD Driver for CUDA version 11.0 is 450.36.06
+    def get_all_configs(
+        self, known_properties: list[VariantPropertyType] | None
+    ) -> list[VariantFeatureConfig]:
+        """Get all supported configurations, including those not in the known
+        properties."""
+        if known_properties is None:
+            return []
+
+        prop_values: dict[VariantFeatureName, list[VariantFeatureValue]] = defaultdict(
+            list
+        )
+        for vprop in known_properties:
+            prop_values[vprop.feature].append(vprop.value)
+
         return [
-            VariantFeatureConfig(  # generates 450k values - temporary idea
-                name=DRIVER_KEY,
-                values=[
-                    f"{major}.{minor:02d}.{patch:02d}"
-                    for major in range(450, 600)
-                    for minor in range(200)
-                    for patch in range(15)
-                ],
-            ),
-            VariantFeatureConfig(
-                name=CUDA_KEY,
-                values=(
-                    [
-                        f"{major}.{minor}"
-                        for major in LATEST_CUDA_MINOR_VERSIONS
-                        for minor in range(LATEST_CUDA_MINOR_VERSIONS[major] + 1)
-                    ]
-                    + [str(major) for major in LATEST_CUDA_MINOR_VERSIONS]
-                ),
-            ),
-            VariantFeatureConfig(
-                name=SM_ARCH_KEY, values=[f"sm_{major}" for major in range(50, 110)]
-            ),
-            VariantFeatureConfig(
-                name=GPU_FAMILY_KEY,
-                values=[s.lower() for s in NVIDIA_GPU_ARCHITECTURE.values()],
-            ),
+            VariantFeatureConfig(name, values) for name, values in prop_values.items()
         ]
-
-
-if __name__ == "__main__":
-    plugin = NvidiaVariantPlugin()
-    print(plugin.get_supported_configs())  # noqa: T201
-    # print(plugin.get_all_configs())
