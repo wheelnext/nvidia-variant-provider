a8fe68361bef52c8cdd1015e1aff23e2662eec46
Jonathan Dekhtiar <jonathan@dekhtiar.com>
2025-10-01 13:52:22 -0400

    Move to stateless Plugin API

--- nvidia_variant_provider/plugin.py
+++ nvidia_variant_provider/plugin.py
@@ -4,7 +4,6 @@ import os
 import warnings
 from dataclasses import dataclass
 from functools import cache
-from functools import cached_property
 from typing import Protocol
 from typing import runtime_checkable
 
@@ -62,21 +61,24 @@ class NvidiaVariantPlugin:
     UMD_MAJOR_RANGE = range(11, 16)
     UMD_MINOR_RANGE = range(21)
 
-    @cached_property
-    def _cuda_environment(self) -> CudaEnvironment | None:
+    @classmethod
+    @cache
+    def _cuda_environment(cls) -> CudaEnvironment | None:
         """Lookup the system to determine the driver / GPU state."""
         return CudaEnvironment.from_system()
 
-    @cache  # noqa: B019
-    def generate_all_umd_values(self) -> list[str]:
+    @classmethod
+    @cache
+    def generate_all_umd_values(cls) -> list[str]:
         return [
             f"{major}.{minor}" if minor is not None else f"{major}"
-            for major in reversed(self.UMD_MAJOR_RANGE)
-            for minor in [*reversed(self.UMD_MINOR_RANGE), None]
+            for major in reversed(cls.UMD_MAJOR_RANGE)
+            for minor in [*reversed(cls.UMD_MINOR_RANGE), None]
         ]
 
-    @cache  # noqa: B019
-    def generate_all_sm_values(self) -> list[str]:
+    @classmethod
+    @cache
+    def generate_all_sm_values(cls) -> list[str]:
         """Generate all possible SM values based on the supported range."""
         return [
             f"{major}{minor}_real"
@@ -89,13 +91,13 @@ class NvidiaVariantPlugin:
         ]
 
     # @property
-    # def kmd_version(self) -> Version | None:
+    # def kmd_version(cls) -> Version | None:
     #     if (
     #       driver_ver := os.environ.get("NV_VARIANT_PROVIDER_FORCE_KMD_DRIVER_VERSION"
     #     ):
     #         return Version(driver_ver)
 
-    #     if (cuda_env := self._cuda_environment) is None:
+    #     if (cuda_env := cls._cuda_environment()) is None:
     #         return None
 
     #     return (
@@ -104,14 +106,15 @@ class NvidiaVariantPlugin:
     #         else None
     #     )
 
-    @property
-    def umd_version(self) -> Version | None:
+    @classmethod
+    @cache
+    def umd_version(cls) -> Version | None:
         if driver_ver := os.environ.get(
             "NV_VARIANT_PROVIDER_FORCE_CUDA_DRIVER_VERSION"
         ):
             return Version(driver_ver)
 
-        if (cuda_env := self._cuda_environment) is None:
+        if (cuda_env := cls._cuda_environment()) is None:
             return None
 
         umd_version = (
@@ -130,15 +133,15 @@ class NvidiaVariantPlugin:
             return None
 
         min_umd_version = Version(
-            f"{min(self.UMD_MAJOR_RANGE)}.{min(self.UMD_MINOR_RANGE)}"
+            f"{min(cls.UMD_MAJOR_RANGE)}.{min(cls.UMD_MINOR_RANGE)}"
         )
         max_umd_version = Version(
-            f"{max(self.UMD_MAJOR_RANGE)}.{max(self.UMD_MINOR_RANGE)}"
+            f"{max(cls.UMD_MAJOR_RANGE)}.{max(cls.UMD_MINOR_RANGE)}"
         )
 
         if min_umd_version > umd_version or umd_version > max_umd_version:
             warnings.warn(
-                f"The UMD version `{self.umd_version}` is outside the supported range: "
+                f"The UMD version `{cls.umd_version}` is outside the supported range: "
                 f"`>={min_umd_version},<={max_umd_version}`.\n"
                 "NVIDIA platform detection has been deactivated.",
                 UserWarning,
@@ -148,22 +151,25 @@ class NvidiaVariantPlugin:
 
         return umd_version
 
-    @property
-    def get_sm_architectures(self) -> tuple[int, int] | None:
+    @classmethod
+    @cache
+    def get_sm_architectures(cls) -> tuple[int, int] | None:
         """Get the SM architectures from the CUDA environment."""
 
         if sm_arch := os.environ.get("NV_VARIANT_PROVIDER_FORCE_SM_ARCH"):
             return tuple(map(int, sm_arch.split(".", maxsplit=1)))  # type: ignore[return-value]
 
-        if self._cuda_environment is None:
+        if (cuda_env := cls._cuda_environment()) is None:
             return None
 
-        return sorted(self._cuda_environment.architectures, reverse=True)[0]
+        return sorted(cuda_env.architectures, reverse=True)[0]
 
-    def get_supported_configs(self) -> list[VariantFeatureConfig]:
+    @classmethod
+    @cache
+    def get_supported_configs(cls) -> list[VariantFeatureConfig]:
         """Filter and sort the properties based on the plugin's logic."""
 
-        if self.umd_version is None:
+        if cls.umd_version is None:
             return []
 
         keyconfigs: list[VariantFeatureConfig] = []
@@ -175,13 +181,11 @@ class NvidiaVariantPlugin:
         # ['12.7', ..., '12.0', '12', '11.20', ..., '11.0', '11']
         umd_values_low = [
             f"{major}.{minor}" if minor is not None else f"{major}"
-            for major in range(
-                self.umd_version.major, min(self.UMD_MAJOR_RANGE) - 1, -1
-            )
+            for major in range(cls.umd_version.major, min(cls.UMD_MAJOR_RANGE) - 1, -1)
             for minor in (
-                [*range(max(self.UMD_MINOR_RANGE), -1, -1), None]
-                if major < self.umd_version.major
-                else [*range(self.umd_version.minor, -1, -1), None]
+                [*range(max(cls.UMD_MINOR_RANGE), -1, -1), None]
+                if major < cls.umd_version.major
+                else [*range(cls.umd_version.minor, -1, -1), None]
             )
         ]
         if umd_values_low:
@@ -197,13 +201,11 @@ class NvidiaVariantPlugin:
         # ['15.20', ..., '15.0', '15', ...,  '13.0', '13', ..., '12.9', '12.8']
         umd_values_low = [
             f"{major}.{minor}" if minor is not None else f"{major}"
-            for major in range(
-                max(self.UMD_MAJOR_RANGE), self.umd_version.major - 1, -1
-            )
+            for major in range(max(cls.UMD_MAJOR_RANGE), cls.umd_version.major - 1, -1)
             for minor in (
-                [*range(max(self.UMD_MINOR_RANGE), -1, -1), None]
-                if major > self.umd_version.major
-                else range(max(self.UMD_MINOR_RANGE), self.umd_version.minor, -1)
+                [*range(max(cls.UMD_MINOR_RANGE), -1, -1), None]
+                if major > cls.umd_version.major
+                else range(max(cls.UMD_MINOR_RANGE), cls.umd_version.minor, -1)
             )
         ]
         if umd_values_low:
@@ -215,7 +217,7 @@ class NvidiaVariantPlugin:
             )
 
         # Priority 3 - NVIDIA SM Arch Compatibility - based on CMAKE Flags
-        if sm_arch := self.get_sm_architectures:
+        if sm_arch := cls.get_sm_architectures:
             # For now we only use the highest SM architecture on the system
             sm_major, sm_minor = sm_arch
 
@@ -245,10 +247,12 @@ class NvidiaVariantPlugin:
 
         return keyconfigs
 
-    def get_all_configs(self) -> list[VariantFeatureConfig]:
+    @classmethod
+    @cache
+    def get_all_configs(cls) -> list[VariantFeatureConfig]:
         """Not used - transparently returns all known properties as configs."""
 
-        all_umd_values = self.generate_all_umd_values()
+        all_umd_values = cls.generate_all_umd_values()
 
         return [
             VariantFeatureConfig(
@@ -261,7 +265,7 @@ class NvidiaVariantPlugin:
             ),
             VariantFeatureConfig(
                 name=NvidiaVariantFeatureKey.SM,
-                values=self.generate_all_sm_values(),
+                values=cls.generate_all_sm_values(),
                 multi_value=True,
             ),
         ]
@@ -272,10 +276,6 @@ if __name__ == "__main__":
 
     os.environ["NV_VARIANT_PROVIDER_FORCE_CUDA_DRIVER_VERSION"] = "12.7"
     os.environ["NV_VARIANT_PROVIDER_FORCE_SM_ARCH"] = "12.5"
-    plugin = NvidiaVariantPlugin()
 
-    # print(plugin.get_supported_configs(None))
-    # print(plugin.get_all_configs(None))
-
-    # May fail if `variantlib` is not installed: not an actual dependency
-    # from variantlib.api import VariantProperty
+    # print(NvidiaVariantPlugin.get_supported_configs(None))
+    # print(NvidiaVariantPlugin.get_all_configs(None))
